189871db6385d3e6f9be13ea4e7b3280ca29d474
yorlend <yorlend@github.com>
2021-12-11 12:38:12 +0300

    lab_07 init

---
+++ lab_07/inc/graph.h
@@ -0,0 +1,17 @@
+#ifndef __GRAPH_H__
+#define __GRAPH_H__
+
+typedef struct node
+{
+    unsigned int key;
+    struct node *next;
+} node_t;
+
+
+typedef struct graph
+{
+    
+} graph_t;
+
+
+#endif /* __GRAPH_H__ */
